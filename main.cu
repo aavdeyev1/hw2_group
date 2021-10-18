018d79210f391125d5d87f2ac3d99fab2c6fd234
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-18 11:38:52 -0700

    123

--- main.cu
+++ main.cu
@@ -202,7 +202,7 @@ int main(int argc, char *argv[]){
                 strcpy(newImageFileName, argv[7]);
 
                 // make GPU filename without .pgm file extension
-                int len = strlen(newImageFileName) - 4;
+                int len = strlen(newImageFileName) - 3;
                 printf("HERE 2: %d\n", len );
                 memcpy(newImageFileNameGPU, &newImageFileName, (strlen(newImageFileName) - 4 )*sizeof(char));
                 printf("HERE 3: %s\n", newImageFileNameGPU);
