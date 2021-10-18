7e020dafd15b7a51e81a7c4568879b17e84a4a61
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-17 19:00:38 -0700

    test

--- pgmProcess.cu
+++ pgmProcess.cu
@@ -35,8 +35,9 @@ __global__ void calcDist(int *pixels, int numRows, int numCols, int centCol, int
     //for comparisons
 
     //use distance function to find the distance
-    float disSq = distanceSquared(p1, p2);
-    pixels[idx] = (int)ceil(disSq);
+    // float disSq = distanceSquared(p1, p2);
+    // pixels[idx] = (int)ceil(disSq);
+    pixels[idx] = idx;
 
     // if (disSq <= rad * rad || ix < numCols) { //if distance is within radius of center point...
     //     pixels[idx] = 0;
